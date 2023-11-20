3e78e87fbf855fb726ba7c63a57e19d5d0ff29f1
rasmussckaletz <rasmus@leeloo.dk>
2023-11-20 14:21:20 +0100

    Update

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-function inputChecker() {
+const word = "about";
 
-    let inputFromHTML = document.getElementById("input").value;
-    let inputSplitArray = inputFromHTML.split("");
+function inputValidation() {
+
+    let inputFromHTML = document.getElementById("input").value.toLowerCase();
+    let validInput = /^[a-z]+$/.test(inputFromHTML);
+    if (validInput && inputFromHTML.length === 5) {
+        return inputFromHTML;
+    } else {
+        alert("test");
+    }
+}
+
+function uiUpdater() {
+
+    let splitSecretWord = word.split("");
+    let validInput = inputValidation();
+    let inputSplitArray = validInput.split("");
     let rowSelector = document.getElementById("row1");
     let count = 0;
 
     for (element of rowSelector.getElementsByTagName("div")) {
         element.innerHTML = inputSplitArray[count];
+        
+        if (splitSecretWord[count] ===  inputSplitArray[count]) {
+            element.style.backgroundColor = "green";
+        } else {
+            element.style.backgroundColor = "red";
+        }
+
         count++;
+
     }
     count = 0;
-
 }
