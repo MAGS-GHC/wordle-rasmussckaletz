5702e20b98bee68e5c2ec54b04a5c697fefb1950
rasmussckaletz <rasmus@leeloo.dk>
2023-11-20 12:09:55 +0100

    Update

--- app.js
+++ app.js
@@ -2,20 +2,13 @@ function inputChecker() {
 
     let inputFromHTML = document.getElementById("input").value;
     let testarray = inputFromHTML.split("");
-    console.log(testarray[0]);
-    /*document.getElementById("row1-column1").innerHTML = testarray[0];
-    document.getElementById("row1-column2").innerHTML = testarray[1];
-    document.getElementById("row1-column3").innerHTML = testarray[2];
-    document.getElementById("row1-column4").innerHTML = testarray[3];
-    document.getElementById("row1-column5").innerHTML = testarray[4];*/
+    let div = document.getElementById('row1');
+    let count = 0;
 
-    var div = document.getElementById('row1');
-    var divs = div.getElementsByTagName('div');
-    var divArray = [];
-    for (var i = 0; i < divs.length; i += 1) {
-        divArray.push(divs[i]);
-        divArray.innerHTML = testarray[i];
+    for (element of div.getElementsByTagName('div')) {
+        element.innerHTML = testarray[count];
+        count++;
     }
+    count = 0;
 
-    console.log(divArray);
 }
